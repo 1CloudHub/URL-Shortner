96b7e824ecc43d0626af7b0aa149fa171cad3827
santhamurthy@1cloudhub.com <107847932+Santhamurthys@users.noreply.github.com>
2023-03-29 11:00:55 +0530

    short_url_stack.py updated

--- short_url/short_url_stack.py
+++ short_url/short_url_stack.py
@@ -22,15 +22,18 @@ class ShortUrlStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        VPC_ID = 'vpc-03a7bf0fff077e4db'
-        VPC_CIDR = '10.30.0.0/16'
-        APIGW_CIDR = '10.3.0.0/16'
-        SUBNET_IDS = ["subnet-0bdb2fd58eb851205", "subnet-010254035fdaf8b9c"]   #subnet-0f0056a9488994519, subnet-08fdb84f20d43f7dd
-        ROUTETABLE_IDS = ["rtb-047756c651cb77d5f", "rtb-047756c651cb77d5f"]
-        FQDN = 'surl1.1cloudhub.com'
-        ACM_CERT_ARN = 'arn:aws:acm:us-east-1:272858488437:certificate/efdefd8e-2301-4c34-8fdf-f3a53146fe62'
+        #Initialising the existing resource details as variables
+        
+        VPC_ID = '<VPC-ID>'                     #Enter your VPC ID
+        VPC_CIDR = '<VPC-CIDR>'                 #Enter your VPC CIDR
+        #APIGW_CIDR = '<API-GATEWAY-CIDR>'      #Enter your API GATEWAY CIDR
+        SUBNET_IDS = ["<SUBNET-ID-2>", "<SUBNET-ID-2>"]               #Enter your Subnet ID's
+        ROUTETABLE_IDS = ["<ROUTE-TABLE-ID-1>", "<ROUTE-TABLE-ID-2>"] #Enter your Route Table ID
+        FQDN = '<FQDN>'                         #Enter your ACM FQDN
+        ACM_CERT_ARN = '<ACM Certificate ARN>'  #Enter your ACM Certificate ARN
 
-        bkt = s3.Bucket(self, "sURL-pub-s3",
+        #Creating S3 Bucket with with auto-delete policies
+        bkt = s3.Bucket(self, "<S3 Bucket Name>",    #Enter Name for S3 Bucket    
                     auto_delete_objects=True,
                     public_read_access=True,
                     removal_policy= removalPolicy.DESTROY,
@@ -42,27 +45,28 @@ class ShortUrlStack(Stack):
                                 }
                                 ])
         
+        #Creating the ACM Certificate
         cert = acm.Certificate. from_certificate_arn(self,'sURL-cert', 
                     ACM_CERT_ARN)
 
+        #Creating Cloudfront Distribution
         dist = cldfrnt.Distribution(self, "sURL-cldfrnt",
                     default_behavior= cldfrnt.BehaviorOptions(
                     origin=origins.HttpOrigin(domain_name=bkt.bucket_website_domain_name)),
                     domain_names=[FQDN],
                     certificate=cert,
                         )
-                
+
+        #Retrieve existing VPC Configurations
         vpcWSubnets = ec2.Vpc.from_vpc_attributes(self,"existing-VPC",
                             vpc_id=VPC_ID,
                             vpc_cidr_block=VPC_CIDR,
                             availability_zones= Fn.get_azs(),
                             private_subnet_ids=SUBNET_IDS,
                             private_subnet_route_table_ids=ROUTETABLE_IDS)
-        # vpcWSubnets = ec2.Vpc.from_lookup(self,"Existing-VPC",
-        #                     vpc_id=VPC_ID, 
-        #                     #subnet_group_name_tag="1CH-PrivateSubnet-1a",
-        #                     )
+        
 
+        # HTTPS Listener and HTTPS Security Group for Lambda Function
         https_sg = ec2.SecurityGroup(self, 
                         "HttpsInbound",
                         vpc=vpcWSubnets,
@@ -76,20 +80,19 @@ class ShortUrlStack(Stack):
                         ec2.Port.tcp(443), 
                         description='allow HTTPS access from API Gateway VPC')
             
-        
+        #Lambda Function to control access
         func = lmbda.Function(self, "sURL-lmbda",
                     runtime=lmbda.Runtime.PYTHON_3_9,
                     code=lmbda.Code.from_asset('./lambda/'),
                     handler='lambda_function.lambda_handler',
                     vpc=vpcWSubnets,
-                    # vpc_subnets=ec2.SubnetSelection(subnet_filters=[ec2.SubnetFilter.by_ids(SUBNET_IDS)]),
                     security_groups=[https_sg],
                     environment={'BUCKET_NAME':bkt.bucket_name,
-                                #'FQDN_NAME':'https://' + dist.distribution_domain_name + '/',
-                                'FQDN_NAME':'https://url.1cloudhub.com/',
+                                'FQDN_NAME':'https://url.domain.com/',  #Enter your domain with url
                                 'LOG_LEVEL':'40'}
                     )
 
+        #Policy that allows the Lambda function to access the S3 bucket hosting the website
         func.role.attach_inline_policy(iam.Policy(self,"allow-sURL-BucketAccess",
                     statements=[iam.PolicyStatement( # Restrict to listing and describing tables
                                     effect=iam.Effect.ALLOW,
@@ -104,6 +107,8 @@ class ShortUrlStack(Stack):
                                     "s3:ListAllMyBuckets"],
                                     resources=[bkt.bucket_arn,
                                     bkt.bucket_arn + "/*",])]))
+        
+        # Optional Part create API Gateway if required
 
         # apiGwy = apg.RestApi(self, "s3URL",
         #             endpoint_types=[apg.EndpointType.REGIONAL,],
